1f746159d72e0274eaf1ae19cb993bab25b02f67
MohWikiDarmawan07 <wiki.error07@gmail.com>
2022-03-09 12:30:21 +0700

    perbaikan

--- pertemuan 3/src/index.js
+++ pertemuan 3/src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM, { render } from 'react-dom';
 import './index.css';
@@ -12,14 +13,13 @@ ReactDOM.render(<Login />, document.getElementById('root'));
 // ReactDOM.render(<HelloComponent />, document.getElementById('root'));
 
 // Statefull Component 
-
 // class StateFullComponent extends React.Component {
-//     render() {
-//         return <p>StateFull Component</p>
-//     }
-// }
+//   render() {
+ //     return <p>StateFull Component</p>
+ //   }
+ //}
 
-// ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
+ //ReactDOM.render(<StateFullComponent />, document.getElementById('root'));
 
 // Stateless Component
 // function HelloComponent() {
@@ -31,4 +31,7 @@ ReactDOM.render(<Login />, document.getElementById('root'));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//ReactDOM.render(<Test />, document.getElementById('root'));
+ReactDOM.render(<HelloComponent />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
 reportWebVitals();
