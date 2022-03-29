cfae931d0a154d2a28c0ec6adee17361da49c99b
MohWikiDarmawan07 <wiki.error07@gmail.com>
2022-03-30 04:24:03 +0700

    menambah fitur auth

--- Pertemuan 4/api/src/container/BlogPost/BlogPost.jsx
+++ Pertemuan 4/api/src/container/BlogPost/BlogPost.jsx
@@ -1,19 +1,32 @@
 import React,{Component} from "react";
 import './BlogPost.css';
+import Post from "../../components/BlogPost/Post";
 
 class BlogPost extends Component{
+state ={  //komponen state dari react untuk statefull component
+listArtikel:[]  //variabel array yang digunakan untuk menyimpan data Api
+
+}
+componentDidMount(){   //komponen mengecek ketika komponen telah di-mount-img, maka panggil api
+    fetch( 'https://jsonplaceholder.typicode.com/posts')
+    .then(Response => Response.json())
+    .then(jsonHasilAmbilDariApi => {
+    this.setState({
+        listArtikel: jsonHasilAmbilDariApi
+    })
+    })
+}
     render() {
         return(
             <div class="post-artikel">
-                <p>Daftar Artikel</p>
-                <div class="artikel">
-                    <img src="http://placeimg.com/80/80/tech" alt="Gambar Thumbnail Artikel" />
-                    </div>
-                    <div class="konten-artikel">
-                        <div class="judul-artikel">Judul Artikel</div>
-                        <p class="isi-artikel">Isi Artikel</p>
-                        </div>
-                </div>
+            <p>Daftar Artikel</p>
+            {
+                this.state.listArtikel.map(artikel => {
+                return<Post judul="JTI polinema" isi="Jurusan teknologi informasi - politeknik negeri malang"/> 
+                })
+            }   
+            </div>
+
             
         )
     }
