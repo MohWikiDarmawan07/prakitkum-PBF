3df3ea23adb3aa2408e8436797af9a89de3d97d7
MohWikiDarmawan07 <wiki.error07@gmail.com>
2022-04-13 09:22:29 +0700

    Uts

---
+++ UTS/router-react/src/data.js
@@ -0,0 +1,39 @@
+const list = [
+    {
+      id: 1,
+      title: "Kulkas 2 pintu",
+      author: "Merk LG tahun 2019",
+      price: 1000000,
+      img: "./image/kulkas1.jpg",
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: "kulkas friezer besar",
+      author: "merk SHARP KELUARAN TERBARU 2022",
+      price: 1200000,
+      img: "./image/Kulkas-Freezer.jpg",
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: "friezer",
+      author: "friezer terbaru 2019",
+      price: 15000000,
+      img: "./image/kulkas dengan frezer.jpg",
+      amount: 1,
+    },
+    {
+      id: 4,
+      title: "kulkas 1 pintu ",
+      author: "merk polytron",
+      price: 850000,
+      img: "./image/polytron 1 pintu.jpg",
+      amount: 1,
+    },
+    
+     
+    
+  ];
+  
+  export default list
