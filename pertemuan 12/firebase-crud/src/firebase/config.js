05388db56555931c528a3bebea9991a618eb4040
MohWikiDarmawan07 <wiki.error07@gmail.com>
2022-06-15 13:14:40 +0700

    firebase crud

---
+++ pertemuan 12/firebase-crud/src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCMpgUWxooWFzLTgLUwzEReS8vTnO3bpHk",
+    authDomain: "fir-login-ae901.firebaseapp.com",
+    databaseURL: "https://fir-login-ae901-default-rtdb.firebaseio.com",
+    projectId: "fir-login-ae901",
+    storageBucket: "fir-login-ae901.appspot.com",
+    messagingSenderId: "585745051934",
+    appId: "1:585745051934:web:10aaca4f94b95a17db190f",
+    measurementId: "G-KB4VBPQ22Y"
+  };
+  export default firebaseConfig;
